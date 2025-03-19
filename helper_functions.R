096dadeac8348335a1b0d8761cbad7f4debef179
Mona Zhu <monajhzhu@gmail.com>
2025-03-19 00:18:36 -0700

    added helper function for repeating bar graphs

---
+++ helper_functions.R
@@ -0,0 +1,27 @@
+bar_graph <- function(
+    df,
+    variable,
+    palette,
+    text_col = "black"
+){
+  df |> 
+    group_by({{ variable }}) |> 
+    summarise(n = n()) |> 
+    ungroup() |> 
+    mutate(Proportion = n/sum(n)) |> 
+    ggplot(aes(x = Proportion, y = "", fill = {{ variable }})) +
+    geom_col() +
+    scale_x_continuous(labels = scales::percent_format(decimal.mark = ",", accuracy = 1)) +
+    geom_text(aes(label = paste0(round(Proportion * 100, 0), "%")),
+              position = position_stack(vjust = 0.5), colour = text_col) +
+    scale_fill_brewer(palette = palette, labels = function(x) str_wrap(x, width = 15),
+                      guide = guide_legend(reverse = TRUE)) +
+    theme_minimal() +
+    theme(
+      legend.position="top"
+    ) +
+    labs(
+      x = "", 
+      y = ""
+    )
+}
